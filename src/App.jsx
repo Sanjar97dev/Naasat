df5a11219ed9c98b335b111fad363f04ecfa3dc2
macbookpro 2022 <macbookpro2016@Macbookpro.local>
2024-08-01 22:22:39 +0600

    update tests

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,10 @@ import Router from './routes/Router';
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
+
 
   return (
-    <div className="app" onClick={handleMenuToggle}>
+    <div className="app">
       <Navbar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
       <Router />
     </div>
